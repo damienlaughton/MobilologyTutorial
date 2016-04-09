44ce0d515c2e413ee360b02215401bb671b917c5
Damien Laughton <dfplaughton@googlemail.com>
2016-04-09 15:23:36 +0100

    Added Core Data Methods to viewcontroller.swift

--- MobilologyDateStampTutorial/ViewController.swift
+++ MobilologyDateStampTutorial/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -18,6 +19,47 @@ class ViewController: UIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Core Data
+  
+  // Insert a new QuoteManagedObject in Core Data
+  func persistNewQuote(price: Double) {
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let moc = appDelegate.managedObjectContext
+    
+    if let quoteEntity = NSEntityDescription.entityForName("QuoteManagedObject", inManagedObjectContext: moc) {
+      
+      if let newQuoteManagedObject = NSManagedObject(entity: quoteEntity,
+                                                    insertIntoManagedObjectContext: moc) as? QuoteManagedObject {
+        
+        newQuoteManagedObject.price = price
+        
+        appDelegate.saveContext()
+      }
+    }
+    
+  }
+  
+  // Get all the quotes that have ever been persisted
+  func retrieveQuotes() -> [QuoteManagedObject] {
+    var retrievedQuotes:[QuoteManagedObject] = []
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let moc = appDelegate.managedObjectContext
+      
+    let fetchRequest = NSFetchRequest(entityName: "QuoteManagedObject")
+    
+    do {
+      let results =
+        try moc.executeFetchRequest(fetchRequest)
+      retrievedQuotes = results as! [QuoteManagedObject]
+    } catch let error as NSError {
+      print("Could not fetch \(error), \(error.userInfo)")
+    }
+    
+    return retrievedQuotes
   }
 
 
