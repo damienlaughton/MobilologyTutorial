e09ebc8a3cf02a399cb0eb79aa329582af6f7503
Damien Laughton <dfplaughton@googlemail.com>
2016-04-18 12:20:36 +0100

    Added MobilologyDoughnutChartTutorial

---
+++ MobilologyDoughnutChartTutorial/MobilologyDoughnutChartTutorial/DoughnutChartView.swift
@@ -0,0 +1,13 @@
+//
+//  DoughnutChartView.swift
+//  MobilologyDoughnutChartTutorial
+//
+//  Created by Damien Laughton on 18/04/2016.
+//  Copyright © 2016 Damien Laughton. All rights reserved.
+//
+
+import UIKit
+
+class DoughnutChartView : UIView {
+  
+}
---
+++ MobilologyDoughnutChartTutorial/MobilologyDoughnutChartTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobilologyDoughnutChartTutorial
+//
+//  Created by Damien Laughton on 18/04/2016.
+//  Copyright © 2016 Damien Laughton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
